c29467e957a5227a196abd41a28da014b20eec66
EstebanRoblero <estebanroblero10@gmail.com>
2025-10-27 01:01:22 -0600

    serivicios dos

--- servicios.py
+++ servicios.py
@@ -15,4 +15,15 @@ class listadeservicios:
             servicio("Pedicure", 20),
             servicio("Base cabello corto y largo",150)
         ]
+
+    #USAMOS BUBBLE SORT
+    def mostrarservicio(self):
+        e=len(self.servicios)
+        for i in range(e):
+            for j in range (0, e - i -1):
+                if self.servicios[j].precio>self.servicios[j + 1].precio:
+                    self.servicios[j], self.servicios[j + 1] =self.servicios[j + 1],self.servicios[j]
+        print("Servicios ordenados por precio")
+
+
     
