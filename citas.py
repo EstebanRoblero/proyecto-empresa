2fd97b508bb1bd567f36c273c1170bd3b4ea9e79
EstebanRoblero <estebanroblero10@gmail.com>
2025-11-03 00:25:44 -0600

    citas 3

--- citas.py
+++ citas.py
@@ -13,4 +13,19 @@ class ListaCitas:
         nueva=Cita(cliente_nombre,fecha,hora)
         if self.cabeza is None:
             self.cabeza=nueva
-        
+        else:
+            actual=self.cabeza
+            while actual.siguiente:
+                actual=actual.siguiente
+            actual.siguiente=nueva
+        print(f"Cita registrada: {cliente_nombre} {fecha} {hora}")
+    
+    def mostrar_citas(self):
+        if self.cabeza is None:
+            print("No hay citas registradas.")
+            return
+        actual=self.cabeza
+        print("\n=== Citas Registradas ===")
+        while actual:
+            print(f"{actual.cliente_nombre} == {actual.fecha} == {actual.hora}")
+            actual=actual.siguiente
