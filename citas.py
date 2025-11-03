2f688ce47d0c8b0f20688d41c97f4730e241cd85
EstebanRoblero <estebanroblero10@gmail.com>
2025-11-03 00:12:01 -0600

    citas uno

---
+++ citas.py
@@ -0,0 +1,8 @@
+class Cita:
+    def __init__(self,cliente_nombre,fecha,hora):
+        self.cliente_nombre=cliente_nombre
+        self.fecha=fecha
+        self.hora=hora
+        self.siguiente=None
+
+        
